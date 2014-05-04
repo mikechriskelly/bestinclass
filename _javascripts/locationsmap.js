1d1a9e1b764be4a52a75b8e33ea62becc661b02b
Mike Kelly <mikechriskelly@gmail.com>
2014-05-03 17:17:31 -0700

    Added links to map infowindow

--- _javascripts/locationsmap.js
+++ _javascripts/locationsmap.js
@@ -10,6 +10,7 @@ function initialize() {
   var isDraggable = $(document).width() > 480 ? true : false;
   var initialZoom = $(document).width() > 480 ? 4 : 3;
 
+  // Set position and interaction controls for map
   var mapOptions = {
     zoom: initialZoom,
     center: new google.maps.LatLng(40, -100),
@@ -17,6 +18,7 @@ function initialize() {
     scrollwheel: false
   };
 
+  // Map marker graphics and options
   var mcOptions = {
     gridsize: 40,
     labelClass: "marker-labels",
@@ -55,23 +57,28 @@ function initialize() {
       position: latlng,
       map: map,
       title: p.Name,
-      icon: "../images/map-icon-green.svg"
+      url: '#' + p.Name,
+      icon: '../images/map-icon-green.svg'
     });
+
+    // Add marker to array
     markers.push(marker);
  
     // Event handler for infowindow on click
     google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(this.title);
+      var content = '<a href="' + this.url + '" class="infowindow-link">' + this.title + '</a>';
+      infowindow.setContent(content);
       infowindow.open(map, this);
     });
   }
   // Cluster nearby markers
   var mc = new MarkerClusterer(map, markers, mcOptions);
-
 }
+
 
 // Async call loads location data from JSON file and then initializes map
 $.getJSON("locations.json", function(response) {
   LOCATIONDATA = response;
   google.maps.event.addDomListener(window, 'load', initialize);
 });
+
